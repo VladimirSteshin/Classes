ecb38f2ae51d5e0bdbf3106b007484180234775e
Vladimir Steshin <warheim24@gmail.com>
2022-03-03 19:11:04 +0300

    2 part complete

--- Classes.py
+++ Classes.py
@@ -7,6 +7,16 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer,
+                      Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'error'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -16,7 +26,9 @@ class Mentor:
 
 
 class Lecturer(Mentor):
-    pass
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
 
 
 class Reviewer(Mentor):
@@ -42,4 +54,11 @@ cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 
+cool_lecturer = Lecturer('Severus', 'Snape')
+cool_lecturer.courses_attached += ['Python']
+best_student.rate_hw(cool_lecturer, 'Python', 5)
+best_student.rate_hw(cool_lecturer, 'Python', 5)
+best_student.rate_hw(cool_lecturer, 'Python', 5)
+
 print(best_student.grades)
+print(cool_lecturer.grades)
